c6ca4c9979961a3c76074d342be4e6fe8149b0c5
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-06 07:36:23 +0530

    Update deploy.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/deploy.sh
+++ src/zerodha/python/deploy.sh
@@ -1,5 +1,7 @@
 ./Token_Generate_Request-Manual_Step.perl
-
+python zerodha_login.py
+python zerodha_login_Automate.py
 nohup python3 algo_trading.py &
 crontab -e
-9 15 * * 1-5 /usr/bin/python3 /path/to/algo_trading.py
+9 15 * * 1-5 /usr/bin/python3 ./algo_trading.py
+#"9 15 * * 1-5 /usr/bin/python3 /path/to/algo_trading.py"
