8b1cfb9190c66a16155149d8e478bc70b3ed6ed7
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-09 19:12:54 +0530

    Update deploy.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/deploy.sh
+++ src/zerodha/python/deploy.sh
@@ -1,3 +1,22 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3 python3-pip -y
+pip install kiteconnect pandas numpy
+pip install pandas numpy scikit-learn
+pip install tensorflow keras pandas numpy scikit-learn
+pip install stable-baselines3 gym numpy pandas
+pip install transformers torch nltk tweepy vaderSentiment
+pip install numpy pandas scikit-learn tensorflow keras cvxpy scipy matplotlib
+pip install numpy pandas tensorflow scikit-learn matplotlib pyod
+
+chmod +x algo_trading.py
+nohup python3 algo_trading.py > output.log 2>&1 &
+ps aux | grep algo_trading.py
+printf "Stop the script if needed bt the command: pkill -f algo_trading.py"
+crontab -e
+18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/username/algo_trading.py > /home/username/output.log 2>&1 &
+tail -f output.log
+printf "Manually restart script if needed : "pkill -f algo_trading.py" then type: "nohup python3 algo_trading.py > output.log 2>&1 &""
+
 ./Token_Generate_Request-Manual_Step.perl
 python zerodha_login.py
 python zerodha_login_Automate.py
