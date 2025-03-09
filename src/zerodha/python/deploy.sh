a227e7df57e28960f6ed3e9a250e5b5d70775d17
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-09 20:09:05 +0530

    Update deploy.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/deploy.sh
+++ src/zerodha/python/deploy.sh
@@ -1,5 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-pip -y
+python3 -m venv TecX
+source TecX/bin/activate
 pip install kiteconnect pandas numpy
 pip install pandas numpy scikit-learn
 pip install tensorflow keras pandas numpy scikit-learn
@@ -13,7 +15,7 @@ nohup python3 algo_trading.py > output.log 2>&1 &
 ps aux | grep algo_trading.py
 printf "Stop the script if needed bt the command: pkill -f algo_trading.py"
 crontab -e
-18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/username/algo_trading.py > /home/username/output.log 2>&1 &
+18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/$USER/algo_trading.py > /home/$USER/output.log 2>&1 &
 tail -f output.log
 printf "Manually restart script if needed : "pkill -f algo_trading.py" then type: "nohup python3 algo_trading.py > output.log 2>&1 &""
 
