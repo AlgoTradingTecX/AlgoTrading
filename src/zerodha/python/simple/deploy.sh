1c8c08819a8c58e882b3296fb74e7db576b64cff
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-10 09:51:33 +0530

    Update deploy.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/simple/deploy.sh
+++ src/zerodha/python/simple/deploy.sh
@@ -16,7 +16,8 @@ ps aux | grep algo_trading.py
 printf "Stop the script if needed bt the command: pkill -f algo_trading.py"
 crontab -e
 18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/$USER/algo_trading.py > /home/$USER/output.log 2>&1 &
-18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /workspace/algo_trading.py > /home/$USER/output.log 2>&1 &
+18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /workspace/AlgoTrading/src/zerodha/python/simple/algo_trading.py > /workspace/AlgoTrading/src/zerodha/python/simple
+//output.log 2>&1 &
 tail -f output.log
 printf "Manually restart script if needed : "pkill -f algo_trading.py" then type: "nohup python3 algo_trading.py > output.log 2>&1 &""
 
