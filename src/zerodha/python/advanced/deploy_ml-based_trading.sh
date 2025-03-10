a7525d42d3976d0c09a0b4a8b3c3393e2b74bce4
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-10 08:12:44 +0530

    Update deploy_ml-based_trading.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced/deploy_ml-based_trading.sh
+++ src/zerodha/python/advanced/deploy_ml-based_trading.sh
@@ -1,4 +1,5 @@
 pip install pandas numpy scikit-learn
 crontab -e
-18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/#USER/algo_trading.py > /home/#USER/output_ml-based_trading.log 2>&1 &
-18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /workspace/AlgoTrading/src/zerodha/python/advanced/ml-based_algo_trading.py > /workspace/AlgoTrading/src/zerodha/python/advanced/output_ml-based_trading.log 2>&1 &
+18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/#USER/ml-based_algo_trading_strategy.py > /home/#USER/output_ml-based_algo_trading_strategy.log 2>&1 &
+18 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /workspace/AlgoTrading/src/zerodha/python/advanced/ml-based_algo_trading_strategy.py > /workspace/AlgoTrading/src/zerodha/python/advanced/output_ml-based_algo_trading_strategy.log 2>&1 &
+tail -f output_ml-based_algo_trading_strategy.log
