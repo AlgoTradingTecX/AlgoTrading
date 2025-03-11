d70778a6c2bb73357e84acfb810f2ea337ae7240
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-11 08:21:25 +0530

    Update aas_dl_rl_algo_trading.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced_ai_strategies/aas_dl_rl_algo_trading.sh
+++ src/zerodha/python/advanced_ai_strategies/aas_dl_rl_algo_trading.sh
@@ -1,2 +1,6 @@
 
 pip install tensorflow keras pandas numpy scikit-learn stable-baselines3 gym
+
+nohup python3 algo_trading_ai.py > output.log 2>&1 &
+crontab -e
+10 9 * * 1-5 pkill -f algo_trading_ai.py && nohup python3 /home/username/algo_trading_ai.py > /home/username/output.log 2>&1 &
