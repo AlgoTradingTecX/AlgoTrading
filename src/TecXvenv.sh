cf025c229604ef27a4ccbcee7ec2c72b7c23920e
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-12 00:11:46 +0530

    Create TecXvenv.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

---
+++ src/TecXvenv.sh
@@ -0,0 +1,11 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3 python3-pip -y
+python3 -m venv TecX
+source TecX/bin/activate
+pip install kiteconnect pandas numpy
+pip install pandas numpy scikit-learn
+pip install tensorflow keras pandas numpy scikit-learn
+pip install stable-baselines3 gym numpy pandas
+pip install transformers torch nltk tweepy vaderSentiment
+pip install numpy pandas scikit-learn tensorflow keras cvxpy scipy matplotlib
+pip install numpy pandas tensorflow scikit-learn matplotlib pyod
