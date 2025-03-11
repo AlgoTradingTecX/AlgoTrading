4ed4a6be69f754dfc7854163a5b370aa5feba8a7
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-11 08:34:58 +0530

    Update aas_dl_rl_algo_trading.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced_ai_strategies/aas_dl_rl_algo_trading.sh
+++ src/zerodha/python/advanced_ai_strategies/aas_dl_rl_algo_trading.sh
@@ -1,9 +1,21 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3 python3-pip -y
+python3 -m venv TecX
+source TecX/bin/activate
 
 pip install tensorflow keras pandas numpy scikit-learn stable-baselines3 gym
 
+chmod +x ml-based_algo_trading_strategy.py
 nohup python3 aas_algo_trading.py > aas_algo_trading_output.log 2>&1 &
-nohup python3 aas_algo_trading.py > _output.log 2>&1 &
+nohup python3 aas_algo_trading.py > aas_algo_trading_output.log 2>&1 &
+
+ps aux | grep ml-based_algo_trading_strategy.py
+printf "Stop the script if needed bt the command: pkill -f ml-based_algo_trading_strategy.py.py"
+
+
 crontab -e
 11 9 * * 1-5 pkill -f aas_algo_trading.py && nohup python3 /home/$USER/aas_algo_trading.py > /home/$USER/aas_algo_trading_output.log 2>&1 &
 
 11 9 * * 1-5 pkill -f aas_algo_trading.py && nohup python3 ./aas_algo_trading.py > ./aas_algo_trading_output.log 2>&1 &
+
+printf "Manually restart script if needed : "pkill -f ml-based_algo_trading_strategy.py" then type: "nohup python3 ml-based_algo_trading_strategy.py.py > output_ml-based_algo_trading_strategy.log 2>&1 &""
