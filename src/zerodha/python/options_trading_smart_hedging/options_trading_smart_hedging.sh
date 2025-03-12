9495a0e9093f4d95efcca54826014f56dcaccf3e
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-12 08:57:53 +0530

    Update options_trading_smart_hedging.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/options_trading_smart_hedging/options_trading_smart_hedging.sh
+++ src/zerodha/python/options_trading_smart_hedging/options_trading_smart_hedging.sh
@@ -1,18 +1,17 @@
 printf"Implement Options Trading Strategies We’ll implement Iron Condor, Straddle, and Spread strategies using Zerodha’s Kite API."
 
 pip install pandas numpy
-chmod +x algo_trading.py
-nohup python3 algo_trading.py > output.log 2>&1 &
-ps aux | grep algo_trading.py
-printf "Stop the script if needed bt the command: pkill -f algo_trading.py"
-crontab -e
-30 9 * * 1-5 nohup python3 options_trading.py > output.log 2>&1 &
+chmod +x sh_ot_stocks_algo_trading.py
+nohup python3 sh_ot_stocks_algo_trading.py > sh_ot_stocks_algo_trading_output.log 2>&1 &
 
-echo "30 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /home/$USER/algo_trading.py > /home/$USER/output.log 2>&1 &"
-30 9 * * 1-5 pkill -f algo_trading.py && nohup python3 /workspace/AlgoTrading/src/zerodha/python/simple/algo_trading.py > /workspace/AlgoTrading/src/zerodha/python/simple
-//output.log 2>&1 &
+printf "Stop the script if needed bt the command: pkill -f sh_ot_stocks_algo_trading.py"
+crontab -e
+30 9 * * 1-5 nohup python3 sh_ot_stocks_algo_trading.py > sh_ot_stocks_algo_trading_output.log 2>&1 &
+ps aux | grep sh_ot_stocks_algo_trading.py
+echo "30 9 * * 1-5 pkill -f ps aux | grep sh_ot_stocks_algo_trading.py && nohup python3 /home/$USER/sh_ot_stocks_algo_trading.py > /home/$USER/sh_ot_stocks_algo_trading_output.log 2>&1 &"
+30 9 * * 1-5 pkill -f sh_ot_stocks_algo_trading.py && nohup python3 /workspace/AlgoTrading/src/zerodha/python//sh_ot_stocks_algo_trading.py > /workspace/AlgoTrading/src/zerodha/python//sh_ot_stocks_algo_trading_output.log 2>&1 &
 tail -f output.log
-printf "Manually restart script if needed : "pkill -f algo_trading.py" then type: "nohup python3 algo_trading.py > output.log 2>&1 &""
+printf "Manually restart script if needed : "pkill -f sh_ot_stocks_algo_trading.py" then type: "nohup python3 sh_ot_stocks_algo_trading.py > sh_ot_stocks_algo_trading_output.log 2>&1 &""
 
 
 printf"Executes Iron Condor & Smart Hedging automatically at market open."
