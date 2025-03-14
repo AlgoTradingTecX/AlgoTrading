4f25be435653d41cd1fbd821e72fe66f6d5cdd89
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 07:33:05 +0530

    Update smart_stop_loss_ai-based_risk_management_for_algo_trading.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/smart_stop_loss_ai-based_risk_management_for_algo_trading/smart_stop_loss_ai-based_risk_management_for_algo_trading.sh
+++ src/zerodha/python/smart_stop_loss_ai-based_risk_management_for_algo_trading/smart_stop_loss_ai-based_risk_management_for_algo_trading.sh
@@ -1,5 +1,5 @@
 pip install numpy pandas scikit-learn
 crontab -e
-0 9 * * 1-5 nohup python3 .py > output.log 2>&1 &
+0 9 * * 1-5 nohup python3 ai-based_risk_management_and_trade_execution.py > ai-based_risk_management_and_trade_execution_output.log 2>&1 &
 
 # Smart Stop Loss & Risk Management runs daily.
