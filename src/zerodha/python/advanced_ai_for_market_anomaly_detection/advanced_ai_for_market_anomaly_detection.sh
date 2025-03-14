5ecee8b0e4d33329474cd5dcb06fb85de1b2858e
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 16:48:41 +0530

    Update advanced_ai_for_market_anomaly_detection.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced_ai_for_market_anomaly_detection/advanced_ai_for_market_anomaly_detection.sh
+++ src/zerodha/python/advanced_ai_for_market_anomaly_detection/advanced_ai_for_market_anomaly_detection.sh
@@ -1,3 +1,5 @@
 # Advanced AI for Market Anomaly Detection
 # We’ll integrate Machine Learning (ML) & AI to detect market anomalies, insider trading patterns, flash crashes, and unusual activities.
 pip install numpy pandas scikit-learn tensorflow keras matplotlib
+crontab -e
+0 9 * * 1-5 nohup python3 ai_based_automated_anomaly_response.py > ai_based_automated_anomaly_response_output.log 2>&1 &
