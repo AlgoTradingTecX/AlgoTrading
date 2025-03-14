0ea62224a32fc85ae6b16f0de6ee7060784c20c1
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 12:16:37 +0530

    Update ai-based_sentiment_analysis_for_market_prediction.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/ai-based_sentiment_analysis_for_market_prediction/ai-based_sentiment_analysis_for_market_prediction.sh
+++ src/zerodha/python/ai-based_sentiment_analysis_for_market_prediction/ai-based_sentiment_analysis_for_market_prediction.sh
@@ -1,5 +1,7 @@
 # AI-Based Sentiment Analysis for Market Prediction
 # We’ll integrate Natural Language Processing (NLP) & AI to analyze news, social media, and financial reports to predict market trends.
-
 # Step 1: Install Required Libraries
 pip install transformers torch nltk tweepy vaderSentiment
+crontab -e
+0 9 * * 1-5 nohup python3 ai-based_sentiment-driven_trade_execution.py > ai-based_sentiment-driven_trade_execution_output.log 2>&1 &
+# Sentiment-based AI trading runs daily.
