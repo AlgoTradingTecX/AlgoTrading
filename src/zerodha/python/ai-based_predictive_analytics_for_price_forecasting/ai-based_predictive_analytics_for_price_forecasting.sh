1e76c67293af2f7b3e78ff8d588806790b34ba36
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 16:17:22 +0530

    Update ai-based_predictive_analytics_for_price_forecasting.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/ai-based_predictive_analytics_for_price_forecasting/ai-based_predictive_analytics_for_price_forecasting.sh
+++ src/zerodha/python/ai-based_predictive_analytics_for_price_forecasting/ai-based_predictive_analytics_for_price_forecasting.sh
@@ -1,3 +1,6 @@
 # AI-Based Predictive Analytics for Price Forecasting
 # We’ll integrate Deep Learning (LSTM, Transformer models) & AI to forecast stock prices & make data-driven trading decisions.
 pip install numpy pandas scikit-learn tensorflow keras matplotlib
+crontab -e
+0 9 * * 1-5 nohup python3 execute_ai_based_trading_based_on_prediction.py > execute_ai_based_trading_based_on_prediction_output.log 2>&1 &
+
