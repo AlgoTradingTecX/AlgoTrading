9ceee541184e162c1a9860e12559c63535b38c1e
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 10:45:57 +0530

    Create rl_for_ai-based_strategy_adaptation.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

---
+++ src/zerodha/python/reinforcement_learning_for_ai-based_strategy_adaptation/rl_for_ai-based_strategy_adaptation.sh.sh
@@ -0,0 +1,4 @@
+# Reinforcement Learning (RL) for AI-Based Strategy Adaptation
+# We’ll now integrate Deep Reinforcement Learning (DRL) to create an AI that learns & adapts to market conditions dynamically.
+
+pip install stable-baselines3 gym numpy pandas tensorflow
