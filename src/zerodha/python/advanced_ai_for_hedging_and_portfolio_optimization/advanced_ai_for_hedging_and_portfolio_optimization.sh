1c9943df4cda1838ad84c5ab59a9f2a2480a780e
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-14 17:26:57 +0530

    Update advanced_ai_for_hedging_and_portfolio_optimization.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced_ai_for_hedging_and_portfolio_optimization/advanced_ai_for_hedging_and_portfolio_optimization.sh
+++ src/zerodha/python/advanced_ai_for_hedging_and_portfolio_optimization/advanced_ai_for_hedging_and_portfolio_optimization.sh
@@ -1,3 +1,5 @@
 # Advanced AI for Hedging & Portfolio Optimization
 # We’ll integrate AI-driven risk management, dynamic hedging, and portfolio optimization to minimize losses and maximize returns.
 pip install numpy pandas scikit-learn tensorflow keras cvxpy scipy matplotlib
+crontab -e
+0 9 * * 1-5 nohup python3 ai-based_automated_hedging_execution.py > ai-based_automated_hedging_execution_output.log 2>&1 &
