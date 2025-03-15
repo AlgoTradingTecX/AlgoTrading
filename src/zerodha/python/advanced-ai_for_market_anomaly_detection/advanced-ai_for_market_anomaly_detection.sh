02a6f839f5455f989e7e30393ee97ca0c4baf907
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-15 08:23:57 +0530

    Update advanced-ai_for_market_anomaly_detection.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/advanced-ai_for_market_anomaly_detection/advanced-ai_for_market_anomaly_detection.sh
+++ src/zerodha/python/advanced-ai_for_market_anomaly_detection/advanced-ai_for_market_anomaly_detection.sh
@@ -2,5 +2,6 @@
 # We’ll use AI-based anomaly detection to identify unusual market patterns, price manipulations, and irregular trading behaviors.
 
 pip install numpy pandas tensorflow scikit-learn matplotlib pyod
-
+crontab -e
 # PyOD (Python Outlier Detection) will help detect anomalies.
+0 9 * * 1-5 nohup python3  > output.log 2>&1 &
