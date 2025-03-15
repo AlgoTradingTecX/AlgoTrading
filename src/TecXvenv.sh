8320aacee38d11b774bba3e4dec92ed69364562f
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-15 17:01:41 +0530

    Update TecXvenv.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/TecXvenv.sh
+++ src/TecXvenv.sh
@@ -9,5 +9,6 @@ pip install stable-baselines3 gym numpy pandas
 pip install transformers torch nltk tweepy vaderSentiment
 pip install numpy pandas scikit-learn tensorflow keras cvxpy scipy matplotlib
 pip install numpy pandas tensorflow scikit-learn matplotlib pyod
+pip install -r requirements.txt
 chmod +x /src/zerodha/python/simple/deploy.sh
 src/zerodha/python/simple/deploy.sh
