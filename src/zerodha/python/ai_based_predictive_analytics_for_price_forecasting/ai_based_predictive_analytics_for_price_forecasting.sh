555aec6b8d2db1bff25c7003861acd7c367d9b85
TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>
2025-03-15 07:54:42 +0530

    Update ai_based_predictive_analytics_for_price_forecasting.sh
    Signed-off-by: TecX Algo Trading admin <ALGOTRADINGTECX@GMAIL.COM>

--- src/zerodha/python/ai_based_predictive_analytics_for_price_forecasting/ai_based_predictive_analytics_for_price_forecasting.sh
+++ src/zerodha/python/ai_based_predictive_analytics_for_price_forecasting/ai_based_predictive_analytics_for_price_forecasting.sh
@@ -1,4 +1,7 @@
-AI-Based Predictive Analytics for Price Forecasting
-We’ll use Deep Learning & AI models to predict stock prices based on historical data, technical indicators, and market patterns.
+# AI-Based Predictive Analytics for Price Forecasting
+# We’ll use Deep Learning & AI models to predict stock prices based on historical data, technical indicators, and market patterns.
+
 pip install numpy pandas tensorflow scikit-learn matplotlib
+
+crontab -e
 0 9 * * 1-5 nohup python3 automate_ai-based_predictive_analytics_for_price_forecasting.py > automate_ai-based_predictive_analytics_for_price_forecasting_output.log 2>&1 &
